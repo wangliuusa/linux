06052542fabc1a1ff62a6988000a27afef2f36d5
ryan_chen <ryan_chen@aspeedtech.com>
2022-03-21 13:44:16 +0800

    clk:ast2600 bclk is come from epll
    Signed-off-by: ryan_chen <ryan_chen@aspeedtech.com>
    Change-Id: I8811e74c9db5473ecdb94ce68fccf594a6825d05
    (cherry picked from commit b2aea6a4132b0216de74af695f69fbd94fcc8eef)

--- drivers/clk/clk-ast2600.c
+++ drivers/clk/clk-ast2600.c
@@ -765,7 +765,7 @@ static int aspeed_g6_clk_probe(struct platform_device *pdev)
 #endif
 
 	/* P-Bus (BCLK) clock divider */
-	hw = clk_hw_register_divider_table(dev, "bclk", "hpll", 0,
+	hw = clk_hw_register_divider_table(dev, "bclk", "epll", 0,
 			scu_g6_base + ASPEED_G6_CLK_SELECTION1, 20, 3, 0,
 			ast2600_div_table,
 			&aspeed_g6_clk_lock);
