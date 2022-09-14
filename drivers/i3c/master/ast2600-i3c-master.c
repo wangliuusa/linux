1f35e5e1fab8c05a503de368954e97a5b0b05d82
Billy Tsai <billy_tsai@aspeedtech.com>
2022-09-14 14:47:58 +0800

    i3c: ast2600: Add the define of response err_type bits 12
    The error type bits 12 of response is set if PEC byte validation error
    occurs in read transfers when TRANSFER_COMMAND[PEC] bit is set to 1.
    This patch also adds the dev_err to print out the response error.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I8d16cb9e8203d0fb35b5caee5c79aa34cfe0e22f

--- drivers/i3c/master/ast2600-i3c-master.c
+++ drivers/i3c/master/ast2600-i3c-master.c
@@ -86,6 +86,7 @@
 #define RESPONSE_ERROR_TRANSF_ABORT	8
 #define RESPONSE_ERROR_I2C_W_NACK_ERR	9
 #define RESPONSE_ERROR_EARLY_TERMINATE	10
+#define RESPONSE_ERROR_PEC_ERR		12
 #define RESPONSE_PORT_TID(x)		(((x) & GENMASK(27, 24)) >> 24)
 #define   TID_SLAVE_IBI_DONE		0b0001
 #define   TID_MASTER_READ_DATA		0b0010
@@ -1015,6 +1016,9 @@ static void aspeed_i3c_master_end_xfer_locked(struct aspeed_i3c_master *master, 
 		case RESPONSE_ERROR_TRANSF_ABORT:
 		case RESPONSE_ERROR_CRC:
 		case RESPONSE_ERROR_FRAME:
+		case RESPONSE_ERROR_PEC_ERR:
+			dev_err(master->dev, "RESPONSE ERROR= %x",
+				xfer->cmds[i].error);
 			ret = -EIO;
 			break;
 		case RESPONSE_ERROR_OVER_UNDER_FLOW:
